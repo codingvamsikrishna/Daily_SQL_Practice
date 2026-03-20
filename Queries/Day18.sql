e5f6221391a81b746a75224650bdb5e97a7778d2
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-03-20 20:59:27 +0530

    Update Day18.sql

--- Queries/Day18.sql
+++ Queries/Day18.sql
@@ -1,2 +1,9 @@
 -- Day 18
 -- Business question:- Find the warehouse with the fastest average delivery time.
+SELECT w.warehouse_id,
+       AVG(DATEDIFF(s.delivery_date, s.order_date)) AS avg_delivery_time
+FROM warehouses w
+JOIN shipments s ON w.warehouse_id = s.warehouse_id
+GROUP BY w.warehouse_id
+ORDER BY avg_delivery_time ASC
+LIMIT 1;
